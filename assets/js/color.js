a87f37acaf1515de7334b1f681b42198a48cdd1c
Greg Wilken <gregwilken@CALLISTO.local>
2017-04-19 18:04:31 -0700

    layout almost finished

---
+++ assets/js/color.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+	$(window).on('load', function () {	
+
+		$('.answers').on('click', function(event){
+			
+			console.log(event);
+
+		
+		})
+
+
+
+
+
+
+
+
+
+		var answer = $('#svgText').find('#group');
+		
+
+	
+
+	});
+
+})
